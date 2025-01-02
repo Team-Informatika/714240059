fa9b9a444b48c2cdd2b9236eeb0534ec5c671691
Ameringkseh <raflimramadhan.204@gmail.com>
2025-01-02 10:57:31 +0700

    bgt

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-import {renderHTML,onclick,window.open} from "<script src="https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/element.js";
+import {renderHTML,onClick} from "https://cdn.jsdelivr.net/gh/jscroot/lib@0.2.0/element.js";
 
 renderHTML('content', 'cok.html');
 
-onclick("github",myGithub);
+onClick("github",myGithub);
 function myGithub() {
     window.open('https://github.com/Ameringkseh', 'blank');
 }
-onclick("whatsapp",mywhatsapp);
+onClick("whatsapp",mywhatsapp);
 function mywhatsapp() {
     window.open('https://whatsapp.com/6287723806103', 'blank');
 }
-onclick("instagram",myinstagram);
+onClick("instagram",myinstagram);
 function myinstagram() {
     window.open('https://instagram.com/not_moch', 'blank');
 }
